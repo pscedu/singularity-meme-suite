5892096bda795faa98a57b92623236ebf4503cbf
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-04-06 22:05:47 -0400

    Added new scripts

--- 5.4.1/generate_binaries.sh
+++ 5.4.1/generate_binaries.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-TOOLS=(alphtype		    fasta-grep		   import-html-template   psp-gen
-ama			    fasta-hamming-enrich   index-fasta-file	  qvalue
-ama-qvalues		    fasta-holdout-set	   iupac2meme		  ramen
-beeml2meme		    fasta-make-index	   jaspar2meme		  ranksum_test
-centrimo-plots		    fasta-most		   llr			  reconcile-tree-alignment
-ceqlogo			    fasta-re-match	   mast_xml_to_html	  reduce-alignment
-chen2meme		    fasta-shuffle-letters  mast_xml_to_txt	  remove-alignment-gaps
-clustalw2fasta		    fasta-subsample	   matrix2meme		  rna2meme
-clustalw2phylip		    fasta-unique-names	   meme-chip_html_to_tsv  rsat-retrieve-seq
-compute-prior-dist	    fisher_exact	   meme-get-motif	  rsat-supported-organisms
-compute-uniform-priors	    fitevd		   meme-rename		  scpd2meme
-create-priors		    gendb		   meme2alph		  sd
-dreme_xml_to_html	    getsize		   meme2images		  sites2meme
-dreme_xml_to_txt	    glam2format		   meme2meme		  streme_xml_to_html
-dtc			    glam2html		   meme_xml_to_html	  taipale2meme
-elm2meme		    glam2mask		   motif-shuffle-columns  tamo2meme
-fasta-center		    glam2psfm		   motiph		  tomtom_xml_to_html
-fasta-dinucleotide-shuffle  glam2scan2html	   nmica2meme		  transfac2meme
-fasta-fetch		    gomo_highlight	   plotgen		  uniprobe2meme
-fasta-from-bed		    hart2meme-bkg	   pmp_bf		  xstreme_html_to_tsv
-fasta-get-markov	    hartemink2psp	   priority2meme)
+TOOLS=(ame  dreme fimo	glam2scan mast meme momo sea streme tomtom centrimo dust glam2	gomo mcast meme-chip purge spamo tgene xstreme)
 
 cat << EOF > template
 #!/bin/bash
@@ -39,7 +19,7 @@ if [ -d /local ]; then
 	OPTIONS=\$OPTIONS" -B /local"
 fi
 
-singularity exec \$OPTIONS \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif \$TOOL "\$@"
+singularity exec \$OPTIONS \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif /opt/bin/\$TOOL "\$@"
 EOF
 
 for TOOL in "${TOOLS[@]}"
